c576dff617f6e951267cf7560df095b79d618775
Muhammad Rifky Abimayu <mexious10@gmail.com>
2019-07-20 12:46:42 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+function biodata(){
+  $biodata = array(
+            'name'  => 'Muhammad Rifky Abimayu',
+            'age' => 17,
+            'address' => 'Jalan Legiun Veteran No.2 Medan Estate',
+            'hobbie' => array(
+                'Menulis',
+                'Menggambar',
+                ),
+            'is_married' => false,
+            'list_school' => array(
+                'sd' => 'SD Swasta Budisatrya',
+                'smp' => 'SMP Negeri 27 Medan',
+                'smk' => 'SMK Tritech Informatika',
+                ),
+            'interest_in_coding' =>  true
+        );
+}
